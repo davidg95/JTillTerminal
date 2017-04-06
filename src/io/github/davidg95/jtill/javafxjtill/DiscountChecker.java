7d7b103626f55bfcd073ebe0aa6f0cf428991ded
David <David@DAVID-PC.JG.Computers.Grant>
2017-04-06 16:34:11 +0100

    - Made changes to discount.

--- src/io/github/davidg95/jtill/javafxjtill/DiscountChecker.java
+++ src/io/github/davidg95/jtill/javafxjtill/DiscountChecker.java
@@ -34,18 +34,18 @@ public class DiscountChecker implements ProductListener {
     public void onProductAdd(ProductEvent pe) {
         for (DiscountBucket b : d.getBuckets()) {
             for (Trigger t : b.getTriggers()) {
-                if (t.getProduct() == pe.getProduct().getId()) {
-                    t.addHit();
-                    if(t.getCurrentQuantity() >= t.getQuantityRequired()){
-                        b.addHit();
-                        t.resetQuantity();
-                        if(b.getCurrentTriggers() >= b.getRequiredTriggers()){
-                            b.reset();
-                            d.addHit();
-                            if(d.getCurrentHits() >= d.getCondition()){
-                                d.reset();
+                if (t.getProduct() == pe.getProduct().getId()) { //Check if the product matches
+                    t.addHit(); //Increase the trigger hit count
+                    if(t.getCurrentQuantity() >= t.getQuantityRequired()){ //Check if the trigger has been triggered
+                        b.addHit(); //If so, increase the bucket hit count
+                        t.resetQuantity(); //Then reset the trigger hit counter
+                        if(b.getCurrentTriggers() >= b.getRequiredTriggers()){ //Check if the bucket has been triggered
+                            d.addHit(); //Add a hit to the discount
+                            b.reset(); //Then, reset the bucket hit count
+                            if(d.getCurrentHits() >= d.getCondition()){ //check if the discount has been triggered
+                                d.reset(); //If so, reset the discount hit count
                                 Platform.runLater(() ->{
-                                    ms.addItemToSale(d);
+                                    ms.addItemToSale(d); //Add the discount to the sale.
                                 });
                             }
                         }
